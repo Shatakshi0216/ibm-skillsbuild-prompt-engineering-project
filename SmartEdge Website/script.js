864981b56635a17f67bb233dda2c3982558e35c4
Shatakshi Tiwari <shatakshitiwari021@gmail.com>
2025-01-18 15:12:44 +0530

    Added

---
+++ SmartEdge Website/script.js
@@ -0,0 +1,4 @@
+function showContactForm() {
+    var contactForm = document.getElementById("contact-form");
+    contactForm.style.display = contactForm.style.display === "block" ? "none" : "block";
+}
